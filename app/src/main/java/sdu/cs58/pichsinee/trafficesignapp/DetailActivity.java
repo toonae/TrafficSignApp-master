1920bb51762022684adb639876ea848910b8a657
supanida <sup.ta1909@gmail.com>
2018-04-04 10:18:46 +0700

    Intent Detail Complete

---
+++ app/src/main/java/sdu/cs58/pichsinee/trafficesignapp/DetailActivity.java
@@ -0,0 +1,33 @@
+package sdu.cs58.pichsinee.trafficesignapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+    //Explicit
+    TextView titleTextView, detailTextView;
+    ImageView signImageView;
+    Button backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        //initial View
+        titleTextView = findViewById(R.id.txvTitle);
+        detailTextView = findViewById(R.id.txvDetail);
+        signImageView = findViewById(R.id.imvSign);
+
+        //รับข้อมูลจากการ Intent ไปแสดงผลบนหน้า XML
+        titleTextView.setText(getIntent().getStringExtra("Title"));
+        detailTextView.setText(getIntent().getStringExtra("Detail"));
+        signImageView.setImageResource(getIntent().getIntExtra("Image",R.drawable.traffic_01));
+
+
+    }//end onCreate Method
+
+}//end Class
---
+++ app/src/main/java/sdu/cs58/pichsinee/trafficesignapp/MainActivity.java
@@ -0,0 +1,49 @@
+package sdu.cs58.pichsinee.trafficesignapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    //Explicit
+    ListView listView;
+    private int[] ints = new int[]{R.drawable.traffic_01,R.drawable.traffic_02,R.drawable.traffic_03,
+            R.drawable.traffic_04,R.drawable.traffic_05,R.drawable.traffic_06,R.drawable.traffic_07,
+            R.drawable.traffic_08,R.drawable.traffic_09,R.drawable.traffic_10,R.drawable.traffic_11,
+            R.drawable.traffic_12,R.drawable.traffic_13,R.drawable.traffic_14,R.drawable.traffic_15,
+            R.drawable.traffic_16,R.drawable.traffic_17,R.drawable.traffic_18,R.drawable.traffic_19,R.drawable.traffic_20};
+
+    private String[] titleStrings, detailStrings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+
+        //ดึงค่าจาก XML ที่ไฟล์ my_content มาแสดงผล
+        titleStrings = getResources().getStringArray(R.array.title);
+        detailStrings = getResources().getStringArray(R.array.detail);
+
+        MyAdapter myAdapter = new MyAdapter(MainActivity.this, ints, titleStrings, detailStrings);
+        listView.setAdapter(myAdapter);
+
+        //เมื่อคลิก item ใน listview จะเชื่อมโยงไปหน้า detail
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent detailIntent = new Intent(MainActivity.this,DetailActivity.class);
+                detailIntent.putExtra("Title",titleStrings[i]);
+                detailIntent.putExtra("Detail",detailStrings[i]);
+                detailIntent.putExtra("Image",ints[i]);
+                startActivity(detailIntent);
+
+            }
+            });
+    }//end onCreate Method
+
+}//end Class
